5c62a85a4907c68f7e921a0d1fd1f711562b75d7
Hugnd <hungnd1112006@gmail.com>
2025-10-04 16:02:05 +0700

    Add files via upload

---
+++ QlyNguoiDung.sql
@@ -0,0 +1,11 @@
+﻿--Tao Database
+CREATE DATABASE QlyNguoiDung
+
+--Tao bang Users
+CREATE TABLE Users
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL,
+    Email NVARCHAR(100) NOT NULL
+)
