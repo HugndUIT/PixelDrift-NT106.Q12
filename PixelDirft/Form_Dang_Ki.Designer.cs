22a2b5cc0b74210645107db1fc41c18b9aa2b12c
Hugnd <hungnd1112006@gmail.com>
2025-10-05 08:54:39 +0700

    Thay đổi giao diện form đăng kí

--- PixelDirft/Form_Dang_Ki.Designer.cs
+++ PixelDirft/Form_Dang_Ki.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PixelDirft
+namespace PixelDirft
 {
     partial class FormDangKi
     {
@@ -43,55 +43,56 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.Salmon;
-            label1.Location = new Point(107, 9);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Orange;
+            label1.Location = new Point(117, 9);
             label1.Name = "label1";
-            label1.Size = new Size(105, 32);
+            label1.Size = new Size(95, 28);
             label1.TabIndex = 0;
             label1.Text = "Đăng Kí";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semilight", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.OrangeRed;
             label2.Location = new Point(12, 57);
             label2.Name = "label2";
-            label2.Size = new Size(155, 17);
+            label2.Size = new Size(155, 16);
             label2.TabIndex = 1;
             label2.Text = "Email hoặc số điện thoại *";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semilight", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.OrangeRed;
             label3.Location = new Point(12, 103);
             label3.Name = "label3";
-            label3.Size = new Size(101, 17);
+            label3.Size = new Size(100, 16);
             label3.TabIndex = 2;
             label3.Text = "Tên đăng nhập *";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Semilight", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.OrangeRed;
             label4.Location = new Point(12, 149);
             label4.Name = "label4";
-            label4.Size = new Size(70, 17);
+            label4.Size = new Size(71, 16);
             label4.TabIndex = 3;
             label4.Text = "Mật khẩu *";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI Semilight", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label5.ForeColor = Color.OrangeRed;
             label5.Location = new Point(12, 195);
             label5.Name = "label5";
-            label5.Size = new Size(126, 17);
+            label5.Size = new Size(125, 16);
             label5.TabIndex = 4;
             label5.Text = "Xác nhận mật khẩu *";
             // 
@@ -132,9 +133,10 @@
             // btn_xacnhan
             // 
             btn_xacnhan.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_xacnhan.Location = new Point(107, 244);
+            btn_xacnhan.ForeColor = Color.DarkOrange;
+            btn_xacnhan.Location = new Point(117, 245);
             btn_xacnhan.Name = "btn_xacnhan";
-            btn_xacnhan.Size = new Size(105, 43);
+            btn_xacnhan.Size = new Size(95, 43);
             btn_xacnhan.TabIndex = 9;
             btn_xacnhan.Text = "Xác nhận";
             btn_xacnhan.UseVisualStyleBackColor = true;
@@ -144,6 +146,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGoldenrod;
             ClientSize = new Size(328, 300);
             Controls.Add(btn_xacnhan);
             Controls.Add(tb_xacnhanmk);
@@ -155,6 +158,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            ForeColor = SystemColors.ControlDarkDark;
             Name = "FormDangKi";
             Text = "Form Đăng Kí";
             ResumeLayout(false);
